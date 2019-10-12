c1228ee6c8555ff3037f0d2c8a0dea8df3cb5a4a
Gurgen-MTN <56431077+Gurgen-MTN@users.noreply.github.com>
2019-10-12 12:32:09 +0300

    Add project files.

---
+++ Convert_String/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Convert_String
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(150, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 30);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Sylfaen", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(62, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(630, 71);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button_Run
+            // 
+            this.button_Run.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_Run.Location = new System.Drawing.Point(221, 163);
+            this.button_Run.Name = "button_Run";
+            this.button_Run.Size = new System.Drawing.Size(269, 69);
+            this.button_Run.TabIndex = 2;
+            this.button_Run.Text = "Run";
+            this.button_Run.UseVisualStyleBackColor = true;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 356);
+            this.Controls.Add(this.button_Run);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Run;
+    }
+}
+
---
+++ Convert_String/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Convert_String
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+              string s = "";
+              int num = int.Parse(textBox1.Text);
+              int temp = num;            // частное от деления
+
+              int position = 0;         // позиция в строке
+              int []x =new int[4];      // массив разрядов
+              int div = 1000;           // делитель на разряды
+             
+              int rest;                 // остаток от деления
+              for (int i = 0; i < 4; i++)
+              {
+                rest = temp / div;
+                x[i] = rest;
+                temp = temp - rest * div;
+                div = div / 10;
+              }
+              // Тысячи
+              switch (x[0])
+              {
+                  case 1: s = "Одна тысяча "; break;
+                  case 2: s = "Две тысячи "; break;
+                  case 3: s = "Три тысячи "; break;
+                  case 4: s = "Четыре тысячи "; break;
+                  case 5: s = "Пять тысяч "; break;
+                  case 6: s = "Шесть тысяч "; break;
+                  case 7: s = "Семь тысяч "; break;
+                  case 8: s = "Восемь тысяч "; break;
+                  case 9: s = "Девять тысяч "; break;
+                  default: break;
+              }
+              position = s.Length;
+
+              // Сотни
+              switch (x[1])
+              {
+                  case 1: s +=  "сто "; break;
+                  case 2: s +=  "двести "; break;
+                  case 3: s +=  "триста "; break;
+                  case 4: s +=  "четыреста "; break;
+                  case 5: s +=  "пятьсот "; break;
+                  case 6: s +=  "шестьсот "; break;
+                  case 7: s +=  "семьсот "; break;
+                  case 8: s +=  "восемьсот "; break;
+                  case 9: s +=  "девятьсот "; break;
+                  default: break;
+              }
+              position = s.Length;
+              
+              // Десятки
+              switch (x[2])
+              {
+                  case 1:
+                      // Числа от 10 до 19
+                      switch (x[3])
+                      {
+                          case 0: s +=  "десять "; break;
+                          case 1: s +=  "одиннадцать "; break;
+                          case 2: s +=  "двенадцать "; break;
+                          case 3: s +=  "тринадцать "; break;
+                          case 4: s +=  "четырнадцать "; break;
+                          case 5: s +=  "пятнадцать "; break;
+                          case 6: s +=  "шестнадцать "; break;
+                          case 7: s +=  "семнадцать "; break;
+                          case 8: s +=  "восемнадцать "; break;
+                          case 9: s +=  "девятнадцать "; break;
+                      }
+                      x[3] = 0;  position = s.Length;     break;
+                  case 2: s +=  "двадцать "; break;
+                  case 3: s +=  "тридцать "; break;
+                  case 4: s +=  "сорок ";    break;
+                  case 5: s +=  "пятьдесят "; break;
+                  case 6: s +=  "шестьдесят "; break;
+                  case 7: s +=  "семьдесят ";  break;
+                  case 8: s +=  "восемьдесят "; break;
+                  case 9: s +=  "девяносто ";   break;
+                  default: break;
+              }
+              position = s.Length;
+              // Единицы
+              switch (x[3])
+              {
+                  case 1: s +=  "один "; break;
+                  case 2: s +=  "два "; break;
+                  case 3: s +=  "три "; break;
+                  case 4: s +=  "четыре "; break;
+                  case 5: s +=  "пять "; break;
+                  case 6: s +=  "шесть "; break;
+                  case 7: s +=  "семь "; break;
+                  case 8: s +=  "восемь "; break;
+                  case 9: s +=  "девять "; break;
+                  default: break;
+              }
+              position = s.Length;
+              label1.Text = s;
+
+            }
+            catch { }
+        }
+    }
+}
